0cc3ffe9e37571ab25c1f6017e4e081368676bf5
CLICKATELL\umair.hussain <umair.hussain@clickatell.com>
2021-08-12 21:58:41 -0400

    TECH-105835 Add cases TECH-117595, TECH-117594, TECH-117590, TECH-117589, TECH-117587

--- src/test/java/api/product_lookup/ProductLookupInfoTest.java
+++ src/test/java/api/product_lookup/ProductLookupInfoTest.java
@@ -145,4 +145,85 @@ public class ProductLookupInfoTest extends BaseApiTest {
 //        });
 //    }
 
+    @Test
+    @Description("GET \u200B/public\u200B/v3\u200B/productInfo :: if unknown MSISDN and mtn_ng vendor id is provided then all vendor products should be returned in the response")
+    @TmsLink("TECH-117587")
+    public void testProductLookupIfUnknownMSISDNAndVendorIdIsProvided() {
+        Map <String, String> map = new Hashtable<>();
+        map.put("clientId",ProductLookupClient.TestClient250);
+        map.put("vendorId",ProductLookupClient.Vendor100);
+        map.put("targetIdentifier",ProductLookupClient.Identifier_13);
+
+        getProductInfo(Port.PRODUCT_LOOKUP, Version.V3, map)
+                .then().assertThat().statusCode(SC_OK)
+                .body("[0].publicProduct.id", Matchers.is(Integer.parseInt(ReserveAndTransactClient.ProductAirtel_100)))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_854))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_130));
+    }
+
+
+    @Test
+    @Description("GET \u200B/public\u200B/v3\u200B/productInfo :: if aritel MSISDN and mtn_ng vendorId are provided then only vendor products should be returned in the response")
+    @TmsLink("TECH-117589")
+    public void testProductInfoIfAirtellMSISDNAndVendorIdIsProvided() {
+        Map <String, String> map = new Hashtable<>();
+        map.put("clientId",ProductLookupClient.TestClient250);
+        map.put("vendorId",ProductLookupClient.Vendor100);
+        map.put("targetIdentifier",ProductLookupClient.Identifier_14);
+
+        getProductInfo(Port.PRODUCT_LOOKUP, Version.V3, map)
+                .then().assertThat().statusCode(SC_OK)
+                .body("[0].publicProduct.id", Matchers.is(Integer.parseInt(ReserveAndTransactClient.ProductAirtel_100)))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_854))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_130));
+    }
+
+    @Test
+    @Description("GET \u200B/public\u200B/v3\u200B/productInfo :: if mtn_ng MSISDN and mtn_ng vendorId is provided then all mtn_ng products should be returned in the response")
+    @TmsLink("TECH-117590")
+    public void testProductInfoIfmtn_ngMSISDNAndVendorIdIsProvided() {
+        Map <String, String> map = new Hashtable<>();
+        map.put("clientId",ProductLookupClient.TestClient250);
+        map.put("vendorId",ProductLookupClient.Vendor100);
+        map.put("targetIdentifier",ProductLookupClient.Identifier_15);
+
+        getProductInfo(Port.PRODUCT_LOOKUP, Version.V3, map)
+                .then().assertThat().statusCode(SC_OK)
+                .body("[0].publicProduct.id", Matchers.is(Integer.parseInt(ReserveAndTransactClient.ProductAirtel_100)))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_854))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_130));
+    }
+
+    @Test
+    @Description("GET \u200B/public\u200B/v3\u200B/productInfo :: if mtn_ng MSISDN is provided and vendor_id isn't provided then all mtn_ng products should be returned in the response")
+    @TmsLink("TECH-117594")
+    public void testProductInfoIfmtn_ngMSISDNisProvidedAndVendorIdIsNotProvided() {
+        Map <String, String> map = new Hashtable<>();
+        map.put("clientId",ProductLookupClient.TestClient250);
+        map.put("vendorId",ProductLookupClient.Vendor100);
+        map.put("targetIdentifier",ProductLookupClient.Identifier_15);
+
+        getProductInfo(Port.PRODUCT_LOOKUP, Version.V3, map)
+                .then().assertThat().statusCode(SC_OK)
+                .body("[0].publicProduct.id", Matchers.is(Integer.parseInt(ReserveAndTransactClient.ProductAirtel_100)))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_854))
+                .body("[0].publicProduct.id", Matchers.not(ProductLookupClient.Product_130));
+    }
+
+    @Test
+    @Description("GET \u200B/public\u200B/v3\u200B/productInfo :: if airtel MSISDN and airtel vendor id are provided then all products should be returned in the response")
+    @TmsLink("TECH-117595")
+    public void testProductInfoIfMSISDNandAirTelVendorisProvided() {
+        Map <String, String> map = new Hashtable<>();
+        map.put("clientId",ProductLookupClient.TestClient250);
+        map.put("vendorId",ProductLookupClient.Vendor103);
+        map.put("targetIdentifier",ProductLookupClient.Identifier_14);
+
+        getProductInfo(Port.PRODUCT_LOOKUP, Version.V3, map)
+                .then().assertThat().statusCode(SC_OK)
+                .body("[0].publicProduct.id", Matchers.is(Integer.parseInt(ProductLookupClient.Product_130)))
+                .body("[1].publicProduct.id", Matchers.is(Integer.parseInt(ProductLookupClient.Product_854)))
+                .body("[0].publicProduct.id",Matchers.not(ReserveAndTransactClient.ProductAirtel_100));
+    }
+
 }
